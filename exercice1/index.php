c7238eae24eeb09177e0563de38d442ae2bb086e
yanbert <yanbert@gmail.com>
2023-02-18 16:37:29 +0000

    Mes commentaires dans le code
    <!-- 
    Correcteur: Yannick GOUGUIA
    
    Toute cette partie doit faire partie du fichier header.php
    ===================
    <!DOCTYPE html>
    <html lang="en">
    <head>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
    </head>
    <body>
    
    <h1>BIENVENUE SUR MA PAGE ADACI</h1>
    ==================
    
    il faut bien fornater le code avec les differents fichiers php
    
    -->

--- exercice1/index.php
+++ exercice1/index.php
@@ -1,3 +1,25 @@
+<!-- 
+Correcteur: Yannick GOUGUIA
+
+Toute cette partie doit faire partie du fichier header.php
+===================
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<h1>BIENVENUE SUR MA PAGE ADACI</h1>
+==================
+
+il faut bien fornater le code avec les differents fichiers php
+
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
